8537ba30b1fcd93ac8d0e8400e5a1c3f3c7b6af0
Cesar <cesarwbr@gmail.com>
2020-06-15 22:06:13 -0300

    feat: add close overlay

--- src/index.js
+++ src/index.js
@@ -268,6 +268,8 @@ export default class InputEmoji extends Component {
   }
 
   handleSelectEmoji = (emoji) => {
+    const { keepOpenend } = this.props
+
     this.textInput.current.focus()
 
     this.pasteHtmlAtCaret(this.getImage(emoji))
@@ -275,7 +277,10 @@ export default class InputEmoji extends Component {
     this.textInput.current.focus()
 
     this.emitChange()
-    this.toggleShowPicker()
+
+    if (!keepOpenend) {
+      this.toggleShowPicker()
+    }
   }
 
   getAllEmojisFromText = (text) => {
@@ -435,6 +440,12 @@ export default class InputEmoji extends Component {
           onClick={this.toggleShowPicker}>
           <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width='24' height='24'><path d='M12 0C5.373 0 0 5.373 0 12s5.373 12 12 12 12-5.373 12-12S18.627 0 12 0m0 22C6.486 22 2 17.514 2 12S6.486 2 12 2s10 4.486 10 10-4.486 10-10 10' /><path d='M8 7a2 2 0 1 0-.001 3.999A2 2 0 0 0 8 7M16 7a2 2 0 1 0-.001 3.999A2 2 0 0 0 16 7M15.232 15c-.693 1.195-1.87 2-3.349 2-1.477 0-2.655-.805-3.347-2H15m3-2H6a6 6 0 1 0 12 0' /></svg>
         </button>
+        {showPicker &&
+          <div
+            className='react-input-emoji--overlay'
+            onClick={this.toggleShowPicker}
+          />
+        }
       </div>
     )
   }
@@ -450,6 +461,7 @@ InputEmoji.propTypes = {
   onClick: t.func,
   onFocus: t.func,
   maxLength: t.number,
+  keepOpenend: t.bool,
   // style
   height: t.number,
   borderRadius: t.number,
