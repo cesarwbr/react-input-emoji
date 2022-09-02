18bf15e2fe5f6647dd9baecb81af89fba2222540
Cesar  Alvarenga <cesarwbr@gmail.com>
2022-09-02 10:57:05 -0700

    feat: bump npm version

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,12 @@ module.exports = {
     browser: true
   },
   extends: [
-    "plugin:react/recommended",
     "google",
     "prettier",
     "plugin:react-hooks/recommended"
   ],
   parser: "babel-eslint",
-  plugins: ["react", "react-hooks"],
+  plugins: ["react-hooks"],
   rules: {
     "react-hooks/rules-of-hooks": "warn", // Checks rules of Hooks
     "react-hooks/exhaustive-deps": "warn" // Checks effect dependencies
