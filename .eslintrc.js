fad87bd5fc66a6990cb5069a75ebdea4cc70fb39
Cesar  Alvarenga <cesarwbr@gmail.com>
2022-10-29 15:57:21 -0700

    feat: add custom button property called buttonElement

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,17 +2,13 @@ module.exports = {
   env: {
     browser: true
   },
-  extends: [
-    "google",
-    "prettier",
-    "plugin:react-hooks/recommended"
-  ],
+  extends: ["google", "prettier", "plugin:react-hooks/recommended"],
   parser: "babel-eslint",
-  plugins: ["react-hooks", "react"],
+  plugins: ["react-hooks"],
   rules: {
     "react-hooks/rules-of-hooks": "warn", // Checks rules of Hooks
     "react-hooks/exhaustive-deps": "warn", // Checks effect dependencies,
-    "react/jsx-uses-react": "error",   
-     "react/jsx-uses-vars": "error" 
+    "react/jsx-uses-react": "error",
+    "react/jsx-uses-vars": "error"
   }
 };
